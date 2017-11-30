3df243a65b350edd71f5abf050b88d5c7503b56c
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-11-29 21:12:07 -0600

    Updated to Gogs 0.11.34

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build . -t wkulhanek/gogs:latest
-docker tag wkulhanek/gogs:latest wkulhanek/gogs:11.29
+docker tag wkulhanek/gogs:latest wkulhanek/gogs:11.34
 docker push wkulhanek/gogs:latest
-docker push wkulhanek/gogs:11.29
+docker push wkulhanek/gogs:11.34
