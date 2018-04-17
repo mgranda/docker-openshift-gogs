fc6cdb1c3d7b917a49fd898f1d11d461a7171d3a
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-04-17 09:29:17 -0400

    Added git tag/push VERSION

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ docker build . -t wkulhanek/gogs:${VERSION}
 docker tag wkulhanek/gogs:${VERSION} wkulhanek/gogs:latest
 docker push wkulhanek/gogs:${VERSION}
 docker push wkulhanek/gogs:latest
+git tag ${VERSION}
+git push origin ${VERSION}
