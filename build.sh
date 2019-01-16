9ffb03db76569eee8515ea16859a37c59b533e05
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-01-16 13:18:27 +0100

    Updated to Gogs 0.11.79

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VERSION=11.66
+export VERSION=11.79
 docker build . -t wkulhanek/gogs:${VERSION}
 docker tag wkulhanek/gogs:${VERSION} wkulhanek/gogs:latest
 docker push wkulhanek/gogs:${VERSION}
