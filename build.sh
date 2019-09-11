8f43b79a9c6ec87e7e24f0e4cf84953728eb0dc5
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-09-11 17:02:28 -0400

    Updated to Gogs 0.11.91, moving to quay.io/gpte-devops-automation

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export VERSION=11.86
-docker build . -t wkulhanek/gogs:${VERSION}
-docker tag wkulhanek/gogs:${VERSION} wkulhanek/gogs:latest
-docker push wkulhanek/gogs:${VERSION}
-docker push wkulhanek/gogs:latest
+export VERSION=11.91
+docker build . -t quay.io/gpte-devops-automation/gogs:${VERSION}
+docker tag quay.io/gpte-devops-automation/gogs:${VERSION} quay.io/gpte-devops-automation/gogs:latest
+docker push quay.io/gpte-devops-automation/gogs:${VERSION}
+docker push quay.io/gpte-devops-automation/gogs:latest
 git tag ${VERSION}
 git push origin ${VERSION}
