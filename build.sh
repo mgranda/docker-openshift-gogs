5e4c030426e16ee35cd68ef572d0840742b87ec0
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2020-10-14 14:16:55 -0400

    Updated to Gogs 0.12.3

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VERSION=11.91
+export VERSION=12.3
 docker build . -t quay.io/gpte-devops-automation/gogs:${VERSION}
 docker tag quay.io/gpte-devops-automation/gogs:${VERSION} quay.io/gpte-devops-automation/gogs:latest
 docker push quay.io/gpte-devops-automation/gogs:${VERSION}
